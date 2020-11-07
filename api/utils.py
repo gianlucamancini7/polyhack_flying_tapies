e8735f514b55b03734252c77fdf0559f7036f9ab
Gianluca Mancini <gianlucamancini@cas1-77.109.166-49.init7.net>
2020-11-07 18:31:38 +0100

    utils

--- api/utils.py
+++ api/utils.py
@@ -1,5 +1,7 @@
 SENSOR_TY = ['motion', 'proximity', 'noise']
-ACTUATOR_TY = []
+ACTUATOR_TY = ['doorlock', 'lamp']
+DISCRETE_TY = ['motion', 'noise', 'doorlock']
+CONTINOUS_TY = ['lamp', 'proximity']
 
 
 def is_sensor_ty(s):
@@ -8,3 +10,11 @@ def is_sensor_ty(s):
 
 def is_actuator_ty(s):
     return s in ACTUATOR_TY
+
+
+def is_discrete(s):
+    return s in DISCRETE_TY
+
+
+def is_continous(s):
+    return s in CONTINOUS_TY
