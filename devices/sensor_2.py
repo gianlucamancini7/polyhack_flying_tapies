08b2832aa9113140870b0a7e849f036770d63f29
Patpat98 <patricia.apostol@gmail.com>
2020-11-07 14:10:06 +0100

    sensor 2 and 3

--- devices/sensor_2.py
+++ devices/sensor_2.py
@@ -1,11 +1,29 @@
+#simulating motion sensor
 
+# relevant imports
+
+import numpy as np
+import random
+
+
+
+def simulate_measurement(measurement_happened, no_suspicious_noise=False, suspicious_noise=True):
+
+    if measurement_happened:
+        return random.choice([no_suspicious_noise, suspicious_noise])
+    else:
+        return None
 
 
 
 #send the measurement to api
 def simulate_sensor_2():
 
-    """Function """
+    measurement_happened=random.choice([True, False])
+    outcome=simulate_measurement(measurement_happened)
+
+    return id_, outcome
+
     
     return
 
--- devices/sensor_3.py
+++ devices/sensor_3.py
@@ -1,11 +1,29 @@
 
+#Measures distance between beacon and receiver
+
+
+#relevant imports 
+import numpy as np
+import random 
+
+
+def simulate_measurement(measurement_happened, dist_beacon_receiver):
+
+    if measurement_happened:
+        return dist_beacon_receiver
+    else:
+        return None
 
 
 
 #send the measurement to api
-def simulate_sensor_3():
+def simulate_sensor_2():
 
-    """Function """
+    measurement_happened=random.choice([True, False])
+    outcome=simulate_measurement(measurement_happened)
+
+    return id, outcome
+
     
     return
 
