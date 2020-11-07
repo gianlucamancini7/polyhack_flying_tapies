f2606c27810057a0fd5d8050fecadff00a690b45
skimslozo <ozolsmiks@gmail.com>
2020-11-07 18:03:36 +0100

    added devices class, adapted parser for the new json format

--- api/deviceparser.py
+++ api/deviceparser.py
@@ -1,29 +1,16 @@
 import json
-
-
-class SensorParseError(Exception):
-    """Gets raised when an invalid sensor type is passed"""
-    pass
+import devices
 
 
 class DeviceParser:
 
     def __init__(self, path):
-        self.devDict = json.load(open(path, 'rb'))
+        self.devices = json.load(open(path, 'rb'))
+
+
 
     def genDevices(self):
         res = {}
-        for key in self.devDict.keys():
-            value = self.devDict[key]
-            id = value['id']
-            ty = value['type']
-            if ty == 'motion':
-                res[id] = {'id': id, 'data': [], 'ty': 'motion'}
-            elif ty == 'noise':
-                res[id] = {'id': id, 'data': [], 'ty': 'noise'}
-            elif ty == 'proximity':
-                res[id] = {'id': id, 'data': [], 'ty': 'proximity'}
-            else:
-                raise SensorParseError(
-                    'Sensor type of {} undefined, could not parse'.format(key))
+        for device in self.devDict.keys():
+            res[device['id']] = Device(device['id'], device['type'])
         return res
